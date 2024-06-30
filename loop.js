c14298bcfe7ef3df959f41bd29e318733b9b47c6
Jenish Rana <68888845+jenishjari@users.noreply.github.com>
2024-06-30 16:54:39 +0530

    Delete loop.js

--- loop.js
+++
@@ -1,6 +0,0 @@
-while (true) {
-    console.log("This is an infinite loop");
-    // Add a delay to avoid consuming too much CPU
-    // This will log the message every second
-    setTimeout(() => {}, 1000);
-}
